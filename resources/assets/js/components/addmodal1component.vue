d60a54d71943bf57feb51d546bd503f518571912
tejalmishra88 <tejalmishra88@gmail.com>
2018-05-20 11:21:55 +1000

    changes in addmodal1

--- resources/assets/js/components/addmodal1component.vue
+++ resources/assets/js/components/addmodal1component.vue
@@ -35,7 +35,7 @@ export default
                            axios.post("http://127.0.0.1:8000/savenames1", {
                             'name': this.record,
                         })
-                        .then(response => {
+                        .then(response => { console.log( 'res', response.data)
                             this.$emit('recordadded', data);
                             this.success = "Task Added Successfully...";
                             this.record = '';
